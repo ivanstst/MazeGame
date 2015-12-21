e02f652207c02937b656e0a256011677ff8b3bbb
ivanstst <ivanstst@gmail.com>
2015-12-21 11:14:13 +0200

    maze game

---
+++ MazeGame/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MazeGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Finish_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("Congratulations");
+            Close();
+        }
+        private void MoveToStart()
+        {
+            Point startingPoint = panel1.Location;
+            startingPoint.Offset(10,10);
+            Cursor.Position = PointToScreen(startingPoint);
+
+        }
+
+        private void label4_MouseEnter(object sender, EventArgs e)
+        {
+            MoveToStart();
+        }
+
+        private void label21_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
